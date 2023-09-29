b512089f3b20a0ba4c405ce591ffcaff1d677d7b
haris rehman <harisrehmanchugtai@gmail.com>
2023-09-29 11:03:48 +0500

    Added Entire Backend for deployment

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:3000", "https://inventory-management.vercel.app"],
+    origin: ["http://localhost:3000", "https://product-mang-api.vercel.app"],
     credentials: true
 }));
 app.use(express.urlencoded({extended: false}));
